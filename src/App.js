51cf463e3f53e838a30b9394004e72ce7452a003
Aniruddha Pandey <anirudh.pandev@gmail.com>
2021-04-30 21:17:39 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
 
-function App() {
+const API_ENDPOINT = `http://localhost:5000`;
+const GET_ROUTE = `pharmacy/category/get`;
+const POST_ROUTE = `pharmacy/category/post`;
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  const _cname = useRef(null);
+  const _author = useRef(null);
+
+  useEffect(() => {
+    console.log("use effect is running");
+    fetch(`${API_ENDPOINT}/${GET_ROUTE}`)
+      .then(response => response.json())
+      .then(data => setItems(data))
+      .catch(error => console.warn(error));
+  }, []);
+
+  const submitData = () => {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        c_name: _cname.current.value,
+        author: _author.current.value,
+      }),
+    };
+
+    fetch(`${API_ENDPOINT}/${POST_ROUTE}`, options)
+      .then(response => response.json())
+      .then(data => {
+        setItems(previousItems => [...previousItems, data]);
+      })
+      .catch(error => console.warn(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={styles.container}>
+      <div className="input" style={styles.input}>
+        <label for="c_name">Medicine</label>
+        <input ref={_cname} type="text" id="c_name"></input>
+        <label for="author">Author</label>
+        <input ref={_author} type="text" id="author"></input>
+        <button onClick={submitData}>Submit</button>
+      </div>
+      <div className="output" style={styles.output}>
+        <ul>
+          {items.map((item, key) => (
+            <li key={key}>
+              <span>
+                {item._id}, {item.c_name}, {item.author}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  image: {
+    width: "300px",
+  },
+};
 
 export default App;
